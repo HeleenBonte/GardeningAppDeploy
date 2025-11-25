151086d13aa18cb5b454926c8712af5b0d97a5f0
HeleenBonte <bonteheleen@hotmail.com>
2025-11-25 09:12:09 +0100

    aanmaken controllers (nog niet uitgewerkt)

---
+++ backend/src/main/java/be/vives/ti/backend/controller/CropController.java
@@ -0,0 +1,15 @@
+package be.vives.ti.backend.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(exposedHeaders = "*")
+@RequestMapping("/api/crops")
+public class CropController {
+
+
+
+}
---
+++ backend/src/main/java/be/vives/ti/backend/controller/UserController.java
@@ -0,0 +1,12 @@
+package be.vives.ti.backend.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(exposedHeaders = "*")
+@RequestMapping("/api/users")
+public class UserController {
+}
