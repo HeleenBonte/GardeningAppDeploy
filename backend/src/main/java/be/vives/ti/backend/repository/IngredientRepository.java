99ed10ddb824b4a537d27f4bb3cb47ad71419dbe
HeleenBonte <bonteheleen@hotmail.com>
2025-11-27 20:22:13 +0100

    dependencies added h2 database opzetten

--- backend/src/main/java/be/vives/ti/backend/repository/IngredientRepository.java
+++ backend/src/main/java/be/vives/ti/backend/repository/IngredientRepository.java
@@ -4,4 +4,5 @@ import be.vives.ti.backend.model.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+
 }
